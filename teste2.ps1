80042d6ce97758573fea8b990cabbee01f589e31
Carlos Alfredo Insensee <138136614+carlosisenseee@users.noreply.github.com>
2025-07-11 09:05:14 -0300

    Update teste2.ps1

--- teste2.ps1
+++ teste2.ps1
@@ -4,6 +4,7 @@ Write-Host "MicroSIP instalado"
 
 Write-Host "Instalando Driver 1"
 Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\5 - DRIVER CERTIFICADO\certisign10.6-x64-10.6.exe"  -ArgumentList "/s" -Wait
+# tentar tambem \silent em caso de não funcionar nos drivers
 Write-Host "Driver 1 Instalado"
 
 Write-Host "Instalando Driver 2"
@@ -11,7 +12,7 @@ Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\5 - DRIVER CERTI
 Write-Host "Driver 2 Instalado"
 
 Write-Host "Instalando Driver 3"
-Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\5 - DRIVER CERTIFICADO\SafeSignIC30124-x64-win-tu-admin.exe"  -ArgumentList "/SILENT" -Wait
+Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\5 - DRIVER CERTIFICADO\SafeSignIC30124-x64-win-tu-admin.exe"  -ArgumentList "/s" -Wait
 Write-Host "Driver 3 Instalado"
 
 Write-Host "Instalando Java"
@@ -19,15 +20,16 @@ Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\6 - JAVA\jre-8u4
 Write-Host "Java Instalado"
 
 Write-Host "Instalando TeamViewer"
-Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\7 - NAVEGADORES E PJE TJRS SHODO\04 - TeamViewer_Setup_x64.exe"  -ArgumentList "/SILENT" -Wait
+Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\7 - NAVEGADORES E PJE TJRS SHODO\04 - TeamViewer_Setup_x64.exe"  -ArgumentList "/S" -Wait
 Write-Host "TeamViewer Instalado"
 
 Write-Host "Instalando Bitdefender"
 Start-Process -FilePath "\\fs01\informatica$\PROGRAMA ADVOGADOS\16 - ANTIVIRUS\Bitdefender Business - Uso Geral\epskit_x64_7.9.22.537\epskit_x64.exe" -Wait
+# Se o instalador foi gerado pelo GravityZone, use: 
+# powershell -ArgumentList "--silent"
 Write-Host "Bitdefender instalado"
 
-# Desinstala Revo
-
-Start-Process "C:\Program Files\VS Revo Group\Revo Uninstaller\unins000.exe" -ArgumentList "/SILENT" -Wait
+Write-Host "Desinstalando Revo"
+Start-Process "C:\Program Files\VS Revo Group\Revo Uninstaller\unins000.exe" -ArgumentList "/VERYSILENT /NORESTART" -Wait
 
 Write-Host "Todas as instalações finalizadas com sucesso!"
